d2118c64e845c840418bc9ecd3720fea61b4516c
Shreekanth-Shastri <shreekanth.shastri@gmail.com>
2024-05-04 17:45:10 -0400

    spring-aws-rds and spring-dynamodb

---
+++ spring-dynamodb/README.md
@@ -0,0 +1,51 @@
+# spring-dynamodb-example
+Build CRUD Application using SpringBoot + DynamoDB then Deploy CRUD application to Elastic Beanstalk 
+
+
+--------------------------------------------
+
+## Dependency (Required)
+
+javascript
+<dependency>
+	<groupId>com.amazonaws</groupId>
+	<artifactId>aws-java-sdk-dynamodb</artifactId>
+	<version>1.11.815</version>
+</dependency>
+--------------------------------------------
+
+## Custom Policy
+
+json
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action":   [ "dynamodb:PutItem" ],
+      "Resource": [ "*" ]
+    },
+    {
+      "Effect": "Allow",
+      "Action":   [ "sns:Publish" ],
+      "Resource": [ "*" ]
+    }
+  ]
+}
+--------------------------------------------
+
+## Request JSON
+
+
+{
+	"name":"Shreekanth",
+	"email":"shreekanth@gmail.com",
+	"age":30,
+	"address":{
+		"city":"Bangalore",
+		"state":"karnataka",
+		"pinCode":"560085"
+	}
+}
+
+--------------------------------------------
---
+++ spring-dynamodb/src/main/java/com/example/aws/SpringDynamodbExampleApplication.java
@@ -0,0 +1,48 @@
+package com.example.aws;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.aws.entity.Person;
+import com.example.aws.repository.PersonRepository;
+
+@SpringBootApplication
+@RestController
+public class SpringDynamodbExampleApplication {
+
+    @Autowired
+    private PersonRepository repository;
+
+    @PostMapping("/savePerson")
+    public Person savePerson(@RequestBody Person person) {
+        return repository.addPerson(person);
+    }
+
+    @GetMapping("/getPerson/{personId}")
+    public Person findPerson(@PathVariable String personId) {
+        return repository.findPersonByPersonId(personId);
+    }
+
+    @DeleteMapping("/deletePerson")
+    public String deletePerson(@RequestBody Person person) {
+        return repository.deletePerson(person);
+    }
+
+    @PutMapping("/editPerson")
+    public String updatePerson(@RequestBody Person person) {
+        return repository.editPerson(person);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDynamodbExampleApplication.class, args);
+    }
+
+}
